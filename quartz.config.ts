1b4354c023412da4f38aa435d31b6140390d357f
Zoe Winters <cost_placard748@knowingeyes.com>
2023-09-19 21:42:06 -0700

    update base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "zoewinters.me",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
